3789fddbdb4d057d24a8dcb7b4044425232258ec
Andrew Song <andrew.song.1122@gmail.com>
2020-06-05 10:29:38 -0700

    GitConnected API call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 function App() {
+  const[user, setUser] = useState(null);
+
+  useEffect(() => {
+    $.get('https://gitconnected.com/v1/portfolio/andrewsong1122', (me) => {
+    console.log(typeof me);  
+    setUser(me);
+    });
+  });
+
+  if(!user) {
+    return(<div>loading</div>);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
